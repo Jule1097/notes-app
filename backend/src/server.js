e3634a3eff9e7230ef6c85a71223b26ff471f3d1
Jule1097 <julianibarra1797@gmail.com>
2026-01-11 19:30:26 -0300

    added cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,16 @@
 import express from "express";
+import cors from "cors";
 import notesRouter from "./routes/notes.router.js";
 import userRouter from "./routes/user.router.js";
 
 const server = express();
+
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(",") : "*",
+  credentials: true,
+};
+
+server.use(cors(corsOptions));
 
 server.use(express.json());
 
